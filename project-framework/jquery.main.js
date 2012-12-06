f7644c8c9058bc3f9dedb461de1c5a6dd2bab540
Joel Stewart <j.stewart101010@gmail.com>
2012-12-06 15:47:25 -0800

    Small update to add reference methods

--- project-framework/jquery.main.js
+++ project-framework/jquery.main.js
@@ -1,4 +1,51 @@
 
+	var DataStatus = Backbone.Model.extend({
+		
+	});
+
+	var DataStatuses = Backbone.Collection.extend({
+		model: DataStatus
+	});
+
+	var MainView = Backbone.View.extend({
+		events: {
+			"click form": "addEvent"
+		},
+
+		initialize: function(options) {
+			this.collection.on("add", this.clearInput, this);
+		},
+
+		addEvent: function(e) {
+			e.preventDefault();
+
+			this.collection.create({ text: this.$('div').val() });
+		},
+
+		clearInput: function() {
+			
+		}
+	});
+
+	var SecondaryView = Backbone.View.extend({
+		initialize: function(options) {
+			this.collection.on("add", this.appendStatus, this);
+		},
+
+		appendStatus: function(status) {
+			
+		}
+	});
+
+	$(document).ready(function() {
+		var statuses = new Statuses();
+		new NewStatusView({ el: $('div'), collection: statuses });
+		new StatusesView({ el: $('div'), collection: statuses });
+	});
+
+
+
+
 	var project = {
 	
 	/* ============================================
